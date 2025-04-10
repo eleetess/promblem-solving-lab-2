170c9f9b663dda6d5b18473ca3832f8c2dbe4d7f
eleetess <ericaleetesseo@gmail.com>
2025-04-09 21:38:14 -0500

    java script if and else if

---
+++ part1-conditionals.js
@@ -0,0 +1,29 @@
+//print A,B,C,D or F based on score
+let score=90
+if (score >=90) {
+    let grading = "A";
+    console.log(grading)
+  } 
+else if (score >=80) {
+    let grading="B"
+    console.log(grading)
+}
+else if(score>=70) {
+    let grading="C"
+    console.log(grading)
+}
+else if (score.=60) {
+    let grading="D"
+    console.log(grading)
+}
+else if(score.=50){
+    let grading="F"
+    console.log(grading)
+}
+
+
+//   else if (time < 20) {
+//     greeting = "Good day";
+//   } else {
+//     greeting = "Good evening";
+//   }
