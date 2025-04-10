3f36d0dd4d25c507d8a43187bccca8f06a9991a1
eleetess <ericaleetesseo@gmail.com>
2025-04-10 08:20:42 -0500

    finishing part 1

--- part1-conditionals.js
+++ part1-conditionals.js
@@ -12,15 +12,36 @@ else if(score>=70) {
     let grading="C"
     console.log(grading)
 }
-else if (score.=60) {
+else if (score>=60) {
     let grading="D"
     console.log(grading)
 }
-else if(score.=50){
+else if(score>=50){
     let grading="F"
     console.log(grading)
 }
-
+let quanity = 12
+// Print warning if quantity > 10
+ 
+if (quanity>10) {
+console.warn("only 10 allowed per order");
+console.log(quanity)
+}
+else if(quanity<10) {
+ console.warn("add to cart");
+console.log(quanity)
+}
+  let password="letmein";  
+  //or use prompt//print "access granted"or "access denied"  
+  if(password="letmein"){
+    let password="access granted";
+    console.log(password)
+  }
+  else if(password=x){
+    let password="access denied";
+    console.log(password)
+  }
+                
 
 //   else if (time < 20) {
 //     greeting = "Good day";
