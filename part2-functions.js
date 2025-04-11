4e63e331ca197039dce9460820fa6033778e7b00
eleetess <ericaleetesseo@gmail.com>
2025-04-10 21:00:40 -0500

    function coding

---
+++ part2-functions.js
@@ -0,0 +1,16 @@
+
+function dogAge(humanYears) {   if (humanYears < 0) {
+    return -1;
+    console.log(humanYears)
+}
+ 
+    
+    else if(dogYears) {
+        // Formula to calculate dog age: 10.5 dog years for the first human year, plus 4 dog years for each additional human year
+        let (dogYears) = 10.5;
+        console.log(humanYears)
+    }
+        if (humanYears > 1) {
+            dogYears += (humanYears - 1) * 4;
+            console.log(humanYears)
+        }
