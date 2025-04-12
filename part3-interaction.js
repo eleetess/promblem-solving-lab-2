074151839d4e6b3cc046875e16110039b12fd86f
eleetess <ericaleetesseo@gmail.com>
2025-04-11 20:38:41 -0500

    interactions practice

---
+++ part3-interaction.js
@@ -0,0 +1,12 @@
+function sayHello() {
+ let greeting="hello"
+ let Element=document.getElementById("message");
+ textcontent="hello from Codex"
+}
+
+function showName(Erica) {
+    let Erica=showName
+    let element=document.getElementById("output");
+    let output=showName
+  
+}
