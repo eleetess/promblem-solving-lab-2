407de048ce108e0222af49d2d083ef8c7045bf08
eleetess <ericaleetesseo@gmail.com>
2025-04-16 15:27:09 -0500

    adding arrays for project

--- part4-arrays.js
+++ part4-arrays.js
@@ -1,10 +1,30 @@
 console.log("somethingNew");
+//Problem 1: Favorite Foods
 //Create an array with 5 of your favorite foods.
 //Log the first and last food in the array
 //Add one more food using .push() and log the new array.
-let favoriteFood = ["Jambalaya","Gumbo","Crawfish","Shrimp","Pasta"];
- favoriteFood.push("cheeseCake","pizza");
+let favoriteFood = ["Jambalaya","Gumbo","Crawfish","Shrimp","Pasta"];//why are they white?
+ favoriteFood.push(["cheesecake","pizza"]);
 console.log("Jambalaya")
 console.log("Pasta")
-console.log("cheeseCake","pizza")
+console.log("cheesecake","pizza")
 //Problem 2: Guest List
+//. Create an array with a list of guests.
+let guestList = ["Merry","Frodo","Sam","Pippin"];
+// Remove the first guest using .shift()
+console.log(guestList)
+let removeFromguestList= guestList.shift();
+// Add a new guest at the end using .push()
+guestList.push (["Legolas"]);
+//Log how many guests are coming (.length)
+ let numberOfGuest= guestList.length;
+ console.log("guestList.length");
+ //Problem 3: Weekend Tasks
+ let tasks = ["laundry", "groceries", "coding", "naps"]
+ //Write a function called printTasks() that loops through the array and logs each task
+ function printTaskList(tasks) {
+  ;
+
+  }
+    
+
