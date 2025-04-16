aadc35de14034bf84e1ff28cf15e1a1b7e1fcd61
eleetess <ericaleetesseo@gmail.com>
2025-04-15 20:24:11 -0500

    adding arrays

---
+++ part4-arrays.js
@@ -0,0 +1,10 @@
+console.log("somethingNew");
+//Create an array with 5 of your favorite foods.
+//Log the first and last food in the array
+//Add one more food using .push() and log the new array.
+let favoriteFood = ["Jambalaya","Gumbo","Crawfish","Shrimp","Pasta"];
+ favoriteFood.push("cheeseCake","pizza");
+console.log("Jambalaya")
+console.log("Pasta")
+console.log("cheeseCake","pizza")
+//Problem 2: Guest List
